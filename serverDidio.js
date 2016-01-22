a7cc667eeb8b2bcf5aacc24b234e72b31682347f
__@erick <erickeduardo@systemroot.com.br>
2016-01-22 15:04:20 -0200

    serverIo update

--- serverDidio.js
+++ serverDidio.js
@@ -1,24 +1,46 @@
-
 /**
 *
-* @API RESTFUL
-* @copyright Erick Eduardo
-* @private - GET CEP
+* @Router Config Server
+* @author Erick Eduardo - __@erick
+* @private  CONFIGURATION SERVER
 */
 	var
-			app 				 = require('./config/serverConfig')
-		, io 			  	 = require('./config/serverIo')
-		, express 		 = require('express')
-		, validator  	 = require('validator')
-		, router 			 = express.Router()
-		, routesController 	 = require('./controller/routeController')
-		, securityController = require('./controller/securityController');
+		   express 		= require('express')
+		 , app 	   		= express()
+		 , server  		= require('http').createServer(app)
+     , io         = require('socket.io').listen(server);
 
 
-	app.use('/', router);
+     server.listen(5886);
 
-	router.route('/cep/:id')
-		  .get(securityController,routesController.getCep);
+     io.sockets.on('connection', function(socket){
+            socket.on('send message', function(data){
+              io.sockets.emit('new message', data);
+              //   socket.broadcast.emit('new message', data);
+            })
+     })
 
-	router.route('/send/')
-		  .post(securityController,routesController.postMailer);
+//
+// /**
+// *
+// * @API RESTFUL
+// * @copyright Erick Eduardo
+// * @private - GET CEP
+// */
+// 	var
+// 			app 				 = require('./config/serverConfig')
+// 		, io 			  	 = require('./config/serverIo')
+// 		, express 		 = require('express')
+// 		, validator  	 = require('validator')
+// 		, router 			 = express.Router()
+// 		, routesController 	 = require('./controller/routeController')
+// 		, securityController = require('./controller/securityController');
+//
+//
+// 	app.use('/', router);
+//
+// 	router.route('/cep/:id')
+// 		  .get(securityController,routesController.getCep);
+//
+// 	router.route('/send/')
+// 		  .post(securityController,routesController.postMailer);
