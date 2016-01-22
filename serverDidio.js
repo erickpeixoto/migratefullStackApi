76ab49c35e68bc777fbf57be1790707e5528507f
__@erick <erickeduardo@systemroot.com.br>
2016-01-22 15:40:25 -0200

    Socket.IO - DEV

--- serverDidio.js
+++ serverDidio.js
@@ -17,28 +17,5 @@
 
 	router.route('/cep/:id')
 		  .get(securityController,routesController.getCep);
-
 	router.route('/send/')
 		  .post(securityController,routesController.postMailer);
-
-
-
-			/**
-			*
-			* @Router Config Server
-			* @author Erick Eduardo - __@erick
-			* @private  CONFIGURATION SERVER IO
-			*/
-				var
-					   expressIO 		= require('express')
-					 , appIO 	   		= express()
-					 , server  		= require('http').createServer(appIO)
-			     , io         = require('socket.io').listen(server);
-
-			     server.listen(3333);
-			     io.sockets.on('connection', function(socket){
-			            socket.on('send message', function(data){
-			              io.sockets.emit('new message', data);
-			              //   socket.broadcast.emit('new message', data);
-			            })
-			     })
